51c490e633669e44790f4ddb545b58ed5cc3f013
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-03 11:49:55 -0300

    arrumando a tela de produtos deixando ela 100%

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import styles from "./product.module.css";
-import ContainerProduct from "../../components/ContainerProduct";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import ContainerProduct from "../../components/ContainerProduct";
+import PageTitle from "../../components/PageTitle";
+import useContexts from "../../hooks/useContext";
+import styles from "./product.module.css";
 
 export default function Product() {
   const { id } = useParams();
+  const { setIsLoading } = useContexts();
   const [product, setProduct] = useState(null);
-  const [allProducts, setAllProducts] = useState([]); // State for all products
+  const [remainingProducts, setRemainingProducts] = useState([]); // State for all products
 
   useEffect(() => {
+    setIsLoading(true);
     // Fetch the specific product
     axios
       .get(`https://back-limbo-production.up.railway.app/products/${id}`, {
@@ -23,6 +27,11 @@ export default function Product() {
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
       });
 
     // Fetch all products
@@ -33,45 +42,47 @@ export default function Product() {
         },
       })
       .then((response) => {
-        setAllProducts(response.data);
+        setRemainingProducts(response.data.filter((p) => p.id !== id));
       })
       .catch((error) => {
         console.log(error);
       });
   }, [id]);
 
-  if (!product) {
-    return <div>Carregando produto...</div>;
-  }
-
-  // Filter out the current product from the list of all products
-  const remainingProducts = allProducts.filter((p) => p.id !== product.id);
-
   return (
-    <section className={styles.product}>
-      <div className={styles.boxProduct}>
-        <div className={styles.boxImg}>
-          <img src={product.image} alt={product.name} />
-        </div>
-        <div className={styles.boxInfoProduct}>
-          <div className={styles.titles}>
-            <h1>{product.name}</h1>
-            <hr />
-            <h2>{product.description}</h2>
+    <>
+      <PageTitle text={"Produtos"} />
+      <section className={styles.product}>
+        {" "}
+        <div className={styles.boxProduct}>
+          <div className={styles.boxImg}>
+            <img
+              src={product != null ? product.image : ""}
+              alt={product != null ? product.name : ""}
+            />
           </div>
-          <p className={styles.description}>{product.details}</p>
-          <div className={styles.cardPrice}>
-            <div className={styles.linePrince}>
-              <p>{product.change_points} pontos</p>
-              <FaHeart />
+          <div className={styles.boxInfoProduct}>
+            <div className={styles.titles}>
+              <h1>{product != null ? product.name : "Carregando"}</h1>
+              <hr />
+              <h2>{product != null ? product.description : ""}</h2>
             </div>
-            <button>Resgatar produto</button>
+            <p className={styles.description}>
+              {product != null ? product.details : ""}
+            </p>
+            <div className={styles.cardPrice}>
+              <div className={styles.linePrince}>
+                <p>{product != null ? product.change_points : ""} pontos</p>
+                <FaHeart />
+              </div>
+              <button>Resgatar produto</button>
+            </div>
           </div>
         </div>
-      </div>
-      <h3 className={styles.titleSection}>Mais produtos</h3>
-      <ContainerProduct listItens={remainingProducts} />{" "}
-      {/* Pass the remaining products */}
-    </section>
+        <h3 className={styles.titleSection}>Mais produtos</h3>
+        <ContainerProduct listItens={remainingProducts} />{" "}
+        {/* Pass the remaining products */}
+      </section>
+    </>
   );
 }
