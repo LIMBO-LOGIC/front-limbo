aa4970c8414e7c1a71bff286f6f1df7e7c33b162
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-10-17 23:27:23 -0300

    arrumando menu nav admin

--- src/components/AdminNavMobile/index.jsx
+++ src/components/AdminNavMobile/index.jsx
@@ -1,52 +1,33 @@
 import { PropTypes } from "prop-types";
 import { useEffect, useState } from "react";
-import {
-  FaFlagCheckered,
-  FaGamepad,
-  FaRegHeart,
-  FaShoppingBag,
-  FaUsers,
-} from "react-icons/fa";
 import { Menu, Sidebar, SubMenu } from "react-pro-sidebar";
 import { Link, useNavigate } from "react-router-dom";
-import styles from "./adminNavMobile.module.css";
-import { LiaMedalSolid } from "react-icons/lia";
-import { BiSolidCategory } from "react-icons/bi";
-import formulaEImage from "/assets/logo_formulaE_branca.png";
 import { IoMenu } from "react-icons/io5";
-import { IoIosLogOut } from "react-icons/io";
+import { FiShoppingBag } from "react-icons/fi";
+import styles from "./adminNavMobile.module.css";
+import formulaEImage from "/assets/logo_formulaE_branca.png";
 import useContexts from "../../hooks/useContext";
 
-const ItemMenu = ({ children }) => {
-  return (
-    <li className="ps-menuitem-root css-1t8x7v1">
-      <div
-        className="ps-menu-button"
-        data-testid="ps-menu-button-test-id"
-        tabIndex="0"
-      >
-        <span className="ps-menu-label css-12w9als">{children}</span>
-      </div>
-    </li>
-  );
-};
+const ItemMenu = ({ children }) => (
+  <li className="ps-menuitem-root css-1t8x7v1">
+    <div className="ps-menu-button" tabIndex="0">
+      <span className="ps-menu-label css-12w9als">{children}</span>
+    </div>
+  </li>
+);
+
 ItemMenu.propTypes = {
   children: PropTypes.node,
 };
 
-const ItemSubMenu = ({ children }) => {
-  return (
-    <li className="ps-menuitem-root css-1t8x7v1 pt-sub">
-      <div
-        className="ps-menu-button"
-        data-testid="ps-menu-button-test-id"
-        tabIndex="0"
-      >
-        <span className="ps-menu-label css-12w9als">{children}</span>
-      </div>
-    </li>
-  );
-};
+const ItemSubMenu = ({ children }) => (
+  <li className="ps-menuitem-root css-1t8x7v1 pt-sub">
+    <div className="ps-menu-button" tabIndex="0">
+      <span className="ps-menu-label css-12w9als">{children}</span>
+    </div>
+  </li>
+);
+
 ItemSubMenu.propTypes = {
   children: PropTypes.node,
 };
@@ -54,12 +35,9 @@ ItemSubMenu.propTypes = {
 export default function AdminNavMobile() {
   const navigate = useNavigate();
   const [toggled, setToggled] = useState(false);
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState({});
   const [nameUser, setNameUser] = useState("");
   const { setDataUser } = useContexts();
-  const handleMenuItemClick = (event) => {
-    event.preventDefault();
-  };
 
   useEffect(() => {
     const userStorage = JSON.parse(localStorage.getItem("userStorage"));
@@ -85,10 +63,9 @@ export default function AdminNavMobile() {
       >
         <Menu>
           <SubMenu
-            defaultOpen={false}
             label={
               <div className={styles.userInfo}>
-                <span className={styles.userName}> {nameUser}</span>
+                <span className={styles.userName}>{nameUser}</span>
                 <span className={styles.userPoints}>
                   {user.current_points} pontos
                 </span>
@@ -102,114 +79,34 @@ export default function AdminNavMobile() {
               />
             }
             className={styles.userProfile}
-            onClick={handleMenuItemClick}
           >
-            <ItemSubMenu>
-              <Link
-                className={styles.linkItemProfile}
-                onClick={() => setToggled(!toggled)}
-                to={"/race/favorites"}
-              >
-                {/* <img src={usaFlag} alt="EUA flag" className={styles.flagIcon} /> */}
-                <FaRegHeart />
-
-                <p>Favoritos</p>
-              </Link>
-            </ItemSubMenu>
-            <ItemSubMenu>
-              <Link className={styles.linkItemProfile} onClick={handleLogout}>
-                <IoIosLogOut />
-                <p>Logout</p>
-              </Link>
-            </ItemSubMenu>
-          </SubMenu>
-          <div className="navegation">
-            <ItemMenu>
-              <Link
-                to="/race"
-                className={styles.itemMenu}
-                onClick={() => setToggled(!toggled)}
-              >
-                <BiSolidCategory />
-                <p>Home</p>
-              </Link>
-            </ItemMenu>
-            <ItemMenu>
-              <Link
-                to="/race/teams"
-                className={styles.itemMenu}
-                onClick={() => setToggled(!toggled)}
-              >
-                <FaUsers />
-                <p>Equipes</p>
-              </Link>
-            </ItemMenu>
             <SubMenu
-              defaultOpen={false}
-              label="Corridas"
-              icon={<FaFlagCheckered />}
+              label="Produtos"
+              icon={<FiShoppingBag />}
               className={styles.subMenu}
-              onClick={handleMenuItemClick}
             >
               <ItemSubMenu>
-                <Link
-                  className={styles.linkItem}
-                  to="/race/races"
-                  onClick={() => setToggled(!toggled)}
-                >
-                  <span>Corridas</span>
+                <Link className={styles.linkItem} to="/admin/allProduct">
+                  <span>Geral Produtos</span>
                 </Link>
               </ItemSubMenu>
               <ItemSubMenu>
-                <Link
-                  to={"/race/live-race"}
-                  className={styles.linkItem}
-                  onClick={() => setToggled(!toggled)}
-                >
-                  <span>Corrida ao vivo</span>
+                <Link className={styles.linkItem} to="/admin/creatProduct">
+                  <span>Criar Produtos</span>
                 </Link>
               </ItemSubMenu>
               <ItemSubMenu>
-                <Link
-                  to={"/race/luck-kick"}
-                  className={styles.linkItem}
-                  onClick={() => setToggled(!toggled)}
-                >
-                  <span>Chute da sorte</span>
+                <Link className={styles.linkItem} to="/admin/productList">
+                  <span>Lista Produtos</span>
+                </Link>
+              </ItemSubMenu>
+              <ItemSubMenu>
+                <Link className={styles.linkItem} to="/admin/uptadeProduct">
+                  <span>Atualizar Produtos</span>
                 </Link>
               </ItemSubMenu>
             </SubMenu>
-            <ItemMenu>
-              <Link
-                to="/race/marketplace"
-                className={styles.itemMenu}
-                onClick={() => setToggled(!toggled)}
-              >
-                <FaShoppingBag />
-                <p>Marketplace</p>
-              </Link>
-            </ItemMenu>
-            <ItemMenu>
-              <Link
-                to="/race/quiz"
-                className={styles.itemMenu}
-                onClick={() => setToggled(!toggled)}
-              >
-                <FaGamepad />
-                <p>Quiz</p>
-              </Link>
-            </ItemMenu>
-            <ItemMenu>
-              <Link
-                to="/race/ranking"
-                className={styles.itemMenu}
-                onClick={() => setToggled(!toggled)}
-              >
-                <LiaMedalSolid />
-                <p>Ranking</p>
-              </Link>
-            </ItemMenu>
-          </div>
+          </SubMenu>
         </Menu>
       </Sidebar>
       <nav className={styles.navMobile}>
