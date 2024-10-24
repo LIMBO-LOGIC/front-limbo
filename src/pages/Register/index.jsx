ac9ace2a492be369de4927dd892398626cfc9739
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-10-23 22:47:36 -0300

    registro 9

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -30,6 +30,92 @@ const Register = () => {
     reader.readAsDataURL(file);
   };
 
+  const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (!picture) {
+      setErrorMessage("Por favor, insira uma foto de perfil.");
+      setIsLoading(false);
+      return;
+    }
+
+    if (!nomeCompleto || !username || !email || !dataNascimento || !senha) {
+      setErrorMessage("Por favor, preencha todos os campos.");
+      setIsLoading(false);
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      setErrorMessage("Email inválido.");
+      setIsLoading(false);
+      return;
+    }
+
+    let publicID = `${email.split("@")[0]}-${new Date()
+      .toISOString()
+      .slice(0, 10)}`;
+
+    const fileToBase64 = (file) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    };
+
+    const uploadToCloudinary = (base64Image) => {
+      const formData = new FormData();
+      formData.append("file", base64Image);
+      formData.append("upload_preset", "u38nm7ok");
+      formData.append("folder", "");
+      formData.append("resource_type", "image");
+      formData.append("public_id", publicID);
+
+      return axios.post(
+        "https://api.cloudinary.com/v1_1/drwk6ohcn/image/upload",
+        formData
+      );
+    };
+
+    const file = picture;
+    fileToBase64(file)
+      .then((base64) => uploadToCloudinary(base64))
+      .then(async (response) => {
+        const body = {
+          fullname: nomeCompleto,
+          nickname: username,
+          email: email,
+          birthdate: dataNascimento.replaceAll("-", "/"),
+          password: senha,
+          profile_picture: response.data.secure_url,
+        };
+
+        console.log("Dados enviados para o servidor:", body);
+
+        await axios.post(`${baseUrl}/user/register`, body, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        toast.success("Cadastro realizado com sucesso!");
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.error("Erro de cadastro:", error);
+        toast.error(
+          "Erro ao tentar fazer o cadastro. Tente novamente mais tarde."
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   const handleGoogleRegister = async () => {
     setIsLoading(true);
     try {
@@ -47,7 +133,6 @@ const Register = () => {
     }
   };
 
-  // Função para enviar os dados ao backend após o usuário fornecer a senha no modal
   const handleSubmitWithPassword = async () => {
     if (!senha) {
       toast.error("Por favor, insira uma senha.");
@@ -57,18 +142,13 @@ const Register = () => {
     setIsLoading(true);
 
     const body = {
-      fullname: googleUser.displayName || "Nome Padrão", // Nome completo do usuário
-      nickname: googleUser.email.split("@")[0], // Gerar o nickname a partir do email
-      email: googleUser.email, // Email do Google
-      birthdate: "2006/12/03", // Data de nascimento padrão
-      password: senha, // Senha fornecida pelo usuário
-      profile_picture: googleUser.photoURL || "URL da foto padrão", // URL da foto ou uma base64 se preferir
+      fullname: googleUser.displayName || "Nome Padrão",
+      nickname: googleUser.email.split("@")[0],
+      email: googleUser.email,
+      birthdate: "2006/12/03",
+      password: senha,
+      profile_picture: googleUser.photoURL || "URL da foto padrão",
     };
-
-    console.log(
-      "Dados enviados para o servidor ao registrar com Google:",
-      body
-    );
 
     try {
       await axios.post(`${baseUrl}/user/register`, body, {
@@ -81,17 +161,10 @@ const Register = () => {
       navigate("/login");
     } catch (error) {
       console.error("Erro ao registrar com Google:", error);
-      if (error.response) {
-        console.log("Erro do servidor:", error.response.data);
-        if (error.response.data.message) {
-          toast.error(`Erro de cadastro: ${error.response.data.message}`);
-        }
-      } else {
-        toast.error("Erro ao tentar fazer o cadastro.");
-      }
+      toast.error("Erro ao tentar fazer o cadastro.");
     } finally {
       setIsLoading(false);
-      setShowPasswordModal(false); // Fecha o modal
+      setShowPasswordModal(false);
     }
   };
 
@@ -105,6 +178,7 @@ const Register = () => {
       <div className={styles.main_login}>
         <div className={styles.left_login}>
           <h2 className={styles.login_login}>Registre-se</h2>
+          <h5 className={styles.login_login}>Coloque a sua foto</h5>
 
           <div className={styles.imagem_input}>
             <UploadPhotoUser
@@ -117,26 +191,94 @@ const Register = () => {
           </div>
 
           <div className={styles.card_usuario}>
+            <div className={styles.textfield}>
+              <input
+                required
+                type="text"
+                name="nomeCompleto"
+                placeholder="Nome Completo"
+                value={nomeCompleto}
+                onChange={(e) => setNomeCompleto(e.target.value)}
+                disabled={isLoading}
+              />
+            </div>
+
+            <div className={styles.textfield}>
+              <input
+                required
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                disabled={isLoading}
+              />
+            </div>
+
+            <div className={styles.textfield}>
+              <input
+                required
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                disabled={isLoading}
+              />
+            </div>
+
+            <div className={styles.textfield}>
+              <input
+                required
+                type="date"
+                name="dataNascimento"
+                placeholder="Data de nascimento"
+                value={dataNascimento}
+                onChange={(e) => setDataNascimento(e.target.value)}
+                disabled={isLoading}
+              />
+            </div>
+
+            <div className={styles.textfield}>
+              <input
+                required
+                type="password"
+                name="senha"
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                disabled={isLoading}
+              />
+            </div>
+
             <Link to="/login" className={styles.itemMenu}>
               <p className={styles.conta}>
-                Já tem conta? <strong>Entre aqui!</strong>
+                Já tem conta? <span>Logue aqui</span>
               </p>
             </Link>
 
-            <div className={styles.botao}>
-              <button
-                className={styles.btn_register}
-                onClick={handleGoogleRegister}
-                disabled={isLoading}
-              >
-                Registrar com Google 8
-              </button>
-            </div>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+
+            <button
+              className={styles.btn_login}
+              onClick={handleRegister}
+              disabled={isLoading}
+            >
+              {isLoading ? "Registrando..." : "Registrar"}
+            </button>
+
+            <button
+              className={styles.btn_google}
+              onClick={handleGoogleRegister}
+              disabled={isLoading}
+            >
+              Registrar com Google
+            </button>
           </div>
         </div>
 
         <div className={styles.right_login}>
-          <img src={imagem_direita} alt="imagem_login" />
+          <img src={imagem_direita} className={styles.image} alt="Animação" />
         </div>
 
         {/* Modal de senha */}
