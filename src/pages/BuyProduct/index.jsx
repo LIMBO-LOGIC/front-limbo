d477744abf31fe41b0af1fbe2e08965746f71f36
luyz-softsy <luiz.silva@softsy.io>
2024-11-02 20:00:32 -0300

    Enviando

---
+++ src/pages/BuyProduct/index.jsx
@@ -0,0 +1,9 @@
+import styles from './buyProduct.module.css'
+
+export default function BuyProduct(){
+    return(
+        <section className={`${styles.containerBuy}`}>
+
+        </section>
+    )
+}
