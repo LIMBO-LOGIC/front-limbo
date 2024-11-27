c96f2431d436a269667061ab3f190b35d998abbb
LuizGustavo <luizgustavo.sp2020@gmail.com>
2024-11-27 13:39:05 -0300

    URL alterada

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,9 @@
 const urlChat = 'https://socket-io-limbo-logic.webpubsub.azure.com/';
 const IP_ADRESS_IOT = '40.90.199.82';
 const IP_ADRESS_IOT_TESTE = 'localhost';
-const urlAPIChat = 'https://limbo-logic-app-chat-web.azurewebsites.net/';
-const baseUrl = "https://back-limbo-logic.azurewebsites.net";
-// const baseUrl = "http://localhost:8000";
+const urlAPIChat = 'https://back-limbo.onrender.com/';
+// const baseUrl = "https://back-limbo-logic.azurewebsites.net";
+const baseUrl = "http://localhost:8000";
 // const baseUrl = 'https://back-limbo-production.up.railway.app';
 
 export { urlChat, baseUrl, IP_ADRESS_IOT, IP_ADRESS_IOT_TESTE, urlAPIChat };
